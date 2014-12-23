761ef97daa0d48dc58611901b261587b2fd0c93a
dolon <dolon1090@yahoo.com>
2014-12-23 18:40:01 +0600

    Assignment_ 23-12-2014

---
+++ add.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        Welcome <?php echo $_POST["firstName"];?> <br>
+Your Last Name Is <?php echo $_POST["lastName"];?><br>
+
+And Your E-mail Is <?php echo $_POST["mail"];?>
+
+    </body>
+</html>
